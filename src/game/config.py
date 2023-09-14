c5e6c966d925d93f497620a72071d4eb5fa74dad
Tristan Slater <hello@trslater.ca>
2023-09-14 00:32:04 -0700

    Move config into src

--- src/game/config.py
+++ src/game/config.py
@@ -1,8 +1,12 @@
 from functools import cache
+from pathlib import Path
 
 import tomllib
 
+ROOT_PATH = Path(__file__, "..").resolve()
+
+
 @cache
 def config():
-    with open("game.toml", "rb") as config_file:
+    with open(ROOT_PATH/"preferences.toml", "rb") as config_file:
         return tomllib.load(config_file)
