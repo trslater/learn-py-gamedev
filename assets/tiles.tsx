6c26c107eef694f17a6d0ffc286ef36f287b2135
Tristan Slater <hello@trslater.ca>
2023-09-18 23:35:02 -0700

    Clean up maps

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,20 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Tiles" tilewidth="16" tileheight="32" tilecount="7" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="Tiles" tilewidth="16" tileheight="32" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
   <image width="16" height="16" source="grass.png"/>
- </tile>
- <tile id="4">
-  <image width="16" height="8" source="wall.png"/>
- </tile>
- <tile id="5">
-  <image width="8" height="8" source="boundary.png"/>
- </tile>
- <tile id="6">
-  <image width="16" height="8" source="stairs.png"/>
- </tile>
- <tile id="7">
-  <image width="8" height="8" source="path.png"/>
  </tile>
  <tile id="8">
   <image width="16" height="32" source="crate.png"/>
--- src/game/game.py
+++ src/game/game.py
@@ -18,7 +18,7 @@ class Game(arcade.Window):
         self.camera = None
 
     def setup(self):
-        self.map = arcade.load_tilemap("maps/test3.json", self.pixel_size, {
+        self.map = arcade.load_tilemap("maps/test.json", self.pixel_size, {
             "Walls": {
                 "use_spatial_hash": True,
             },
@@ -29,37 +29,15 @@ class Game(arcade.Window):
         self.player = self.scene.get_sprite_list("Player")[0]
 
         self.physics_engine = arcade.PhysicsEngineSimple(
-            self.player, self.scene.get_sprite_list("Boundaries"))
+            self.player, self.scene.get_sprite_list("Obstacles"))
         
         self.camera = arcade.Camera(self.width, self.height)
 
     def adjust_camera(self):
-        player_left_bound = self.player.center_x - 64*self.pixel_size
-        player_right_bound = self.player.center_x + 64*self.pixel_size
-        player_bottom_bound = self.player.center_y - 64*self.pixel_size
-        player_top_bound = self.player.center_y + 64*self.pixel_size
-        
-        camera_left_bound = self.camera.position.x
-        camera_right_bound = self.camera.position.x + self.camera.viewport_width
-        camera_bottom_bound = self.camera.position.y
-        camera_top_bound = self.camera.position.y + self.camera.viewport_height
+        new_x = self.player.center_x - self.camera.viewport_width/2
+        new_y = self.player.center_y - self.camera.viewport_height/2
 
-        new_x = self.camera.position.x
-        new_y = self.camera.position.y
-
-        if player_left_bound < camera_left_bound:
-            new_x = player_left_bound
-
-        if player_right_bound > camera_right_bound:
-            new_x = player_right_bound - self.camera.viewport_width
-
-        if player_bottom_bound < camera_bottom_bound:
-            new_y = player_bottom_bound
-
-        if player_top_bound > camera_top_bound:
-            new_y = player_top_bound - self.camera.viewport_height
-
-        self.camera.move_to((new_x, new_y), 0.2)
+        self.camera.move_to((new_x, new_y))
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
