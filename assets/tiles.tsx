885dfafdf45abf0af5ee1112f5f5154f1b35e8e4
Tristan Slater <hello@trslater.ca>
2023-09-19 00:07:29 -0700

    Add some more map assets

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Tiles" tilewidth="16" tileheight="32" tilecount="3" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="Tiles" tilewidth="16" tileheight="32" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
   <image width="16" height="16" source="grass.png"/>
@@ -16,5 +16,11 @@
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="25.875" width="15.625" height="5.75"/>
   </objectgroup>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="wall.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="32" source="door.png"/>
  </tile>
 </tileset>
